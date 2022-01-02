f4b3c960ec2346cb37981f317300f7288e743c88
vietpq3 <33243441+vietpq3@users.noreply.github.com>
2022-01-02 19:06:37 +0700

    init

---
+++ src/test/java/viet/teamcitydemo/TeamcityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package viet.teamcitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
